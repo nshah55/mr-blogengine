3d728f26949c3e737257126e7e86d5daea501193
benjamin.lovell <benjamin.lovell@c9b6962a-6a49-0410-9aad-4d85576fd8e9>
2008-04-17 19:37:47 +0000

    Introduced the mapper and first cut at the blog post aggregate. This checkin has failing tests.

--- src/BenLovell.BlogEngine.Tests/Unit/Services/BlogPostServiceFixture.cs
+++ src/BenLovell.BlogEngine.Tests/Unit/Services/BlogPostServiceFixture.cs
@@ -2,18 +2,48 @@ using BenLovell.BlogEngine.Core;
 using BenLovell.BlogEngine.Core.Messages;
 using BenLovell.BlogEngine.Core.Services;
 using MbUnit.Framework;
+using Rhino.Mocks;
 
 namespace BenLovell.BlogEngine.Tests.Unit.Services
 {
 	[TestFixture]
 	public class BlogPostServiceFixture
 	{
+		private MockRepository mockery;
+		private IBlogPost post;
+		private IBlogPostMapper blogPostMapper;
+
+		[SetUp]
+		public void Setup()
+		{
+			mockery = new MockRepository();
+			blogPostMapper = mockery.DynamicMock<IBlogPostMapper>();
+			post = mockery.DynamicMock<IBlogPost>();
+		}
+
 		[Test]
 		public void AddPost_ShouldSavePost_AndBuildResponseWithSuccess()
 		{
-			IBlogPostService service = new BlogPostService();
+			IBlogPostService service = new BlogPostService(blogPostMapper);
 			AddPostResponseDto response = service.AddPost(new AddPostRequestDto());
 			Assert.IsTrue(response.PostAdded, "PostAdded was false!");
+		}
+
+		[Test]
+		public void AddPost_ShouldUseMapperForPost()
+		{
+			AddPostRequestDto requestDto = new AddPostRequestDto();
+			IBlogPostService service = new BlogPostService(blogPostMapper);
+			
+			using(mockery.Record())
+			{
+				Expect.Call(blogPostMapper.MapFrom(requestDto)).Return(post);
+			}
+
+			using(mockery.Playback())
+			{
+				service.AddPost(requestDto);
+			}
 		}
 	}
 }
